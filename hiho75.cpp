d24541276cdd25c73b3614f8d237ceb1fdf17178
Chuang <chuang@ChuangdeMacBook-Pro.local>
2016-01-12 00:18:29 +0800

    hiho75.cpp need complete
    N/A
    
    Signed-off-by: Chuang <chuang@ChuangdeMacBook-Pro.local>

---
+++ hiho75.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int y_num = 0;
+int r_num = 0;
+int b_num = 0;
+
+int main(){
+    string inputStr;
+    getline(cin, inputStr);
+    //cout<<inputStr<<endl;
+
+    const string y_tag = "<yellow>";
+    const string y_end_tag = "</yellow>";
+
+    const string r_tag = "<red>";
+    const string r_end_tag = "</red>";
+
+
+
+    for(int i = 0; i < inputStr.size(); i ++) {
+        
+    }
+}
